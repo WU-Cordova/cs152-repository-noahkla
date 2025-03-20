1ba93a366b9397a6732b27a13adeb01fd9bfafb9
noahkla <noahklarreich@gmail.com>
2025-03-20 11:53:22 -0700

    Update program.py

--- projects/project3/program.py
+++ projects/project3/program.py
@@ -1,16 +1,20 @@
 from datastructures.circularqueue import CircularQueue
 from datastructures.arraystack import ArrayStack
+from tests.test_arraystack import TestArrayStack, stack
 
 
 def main():
     print('hello world')
+  #  t = TestArrayStack()
+    #s = stack()
+    print(t.test_push(s))
     s = ArrayStack()
     s.push(1)
     s.push(2)
     s.push(3)
-    #print(s.peek())
-    s.pop()
-    print(s.pop())
+    ##print(s.peek())
+   # s.pop()
+  #  print(s.pop())
    # q = CircularQueue(5)
     print("Hello, World!")
    # q.enqueue(8)
