2708cfcaefe656e63b6c8caed74fb3fdab792d8c
noahkla <noahklarreich@gmail.com>
2025-04-08 14:43:36 -0700

    Update linkedlist.py

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -15,17 +15,28 @@ class LinkedList[T](ILinkedList[T]):
         previous: Optional[LinkedList.Node] = None
 
     def __init__(self, data_type: type = object) -> None:
-        raise NotImplementedError("LinkedList.__init__ is not implemented")
+        self.head = self.tail = None
+        self.count = 0
 
     @staticmethod
-    def from_sequence(sequence: Sequence[T], data_type: type=object) -> LinkedList[T]:
-        raise NotImplementedError("LinkedList.from_sequence is not implemented")
+    def from_sequence(self, sequence: Sequence[T], data_type: type=object) -> LinkedList[T]:
+        self.head = sequence[0]
+        self.tail = None
 
     def append(self, item: T) -> None:
-        raise NotImplementedError("LinkedList.append is not implemented")
+        new_node = LinkedList.Node(item)
+        new_node.previous = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+        self.count += 1
 
     def prepend(self, item: T) -> None:
-        raise NotImplementedError("LinkedList.prepend is not implemented")
+        new_node = LinkedList.Node(item)
+        self.head.previous = new_node
+        new_node.next = self.head
+        self.head = new_node
+        self.count += 1
+
 
     def insert_before(self, target: T, item: T) -> None:
         raise NotImplementedError("LinkedList.insert_before is not implemented")
@@ -40,22 +51,46 @@ class LinkedList[T](ILinkedList[T]):
         raise NotImplementedError("LinkedList.remove_all is not implemented")
 
     def pop(self) -> T:
-        raise NotImplementedError("LinkedList.pop is not implemented")
+        if self.count == 0:
+            raise IndexError
+        if self.count == 1:
+            data = self.head.data
+            self.head = self.tail = None
+            
+        else:
+            data = self.tail.data
+            self.tail.previous.next = None
+            self.tail = self.tail.previous
+        self.count -= 1
+        return data
+
 
     def pop_front(self) -> T:
-        raise NotImplementedError("LinkedList.pop_front is not implemented")
+        if self.count == 0:
+            raise IndexError
+        data = self.head.data
+        if self.count == 1:
+            data = self.head.data
+            self.head = self.tail = None
+        else:
+            self.head.next.previous = None
+            self.head = self.head.next
+        count -= 1
+        return data
+
+
 
     @property
     def front(self) -> T:
-        raise NotImplementedError("LinkedList.front is not implemented")
+        return self.head.data
 
     @property
     def back(self) -> T:
-        raise NotImplementedError("LinkedList.back is not implemented")
+        return self.tail.data
 
     @property
     def empty(self) -> bool:
-        raise NotImplementedError("LinkedList.empty is not implemented")
+        return self.count == 0
 
     def __len__(self) -> int:
         raise NotImplementedError("LinkedList.__len__ is not implemented")
