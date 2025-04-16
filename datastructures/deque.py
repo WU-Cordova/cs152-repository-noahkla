bfdbb0a6cc491d3c9004d1b5bb52d361d380f33d
noahkla <noahklarreich@gmail.com>
2025-04-16 16:44:18 -0700

    Update deque.py

--- datastructures/deque.py
+++ datastructures/deque.py
@@ -153,7 +153,9 @@ class Deque[T](IQueue[T]):
             - str: A detailed string representation of the deque.
         """
         raise NotImplementedError("Method to get the detailed string representation of the deque is not implemented.")
-
+    @property
+    def count(self):
+        return self.a.count
 
 if __name__ == '__main__':
     filename = os.path.basename(__file__)
